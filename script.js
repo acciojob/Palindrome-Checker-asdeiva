4dfec7bd99f81895f5761d9d2e96b373de92ed5a
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2023-01-23 09:48:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	let splitString = str.split("");
+	let reverseArray = splitString.reverse();
+	let str2 = reverseArray.join("");
+	let flag = false;
+	if(str == str2){
+		flag = true;
+	}
+	return flag;
 }
 module.exports = palindrome
