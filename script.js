5f27dacc8650ecd8413744bfbbce59f490976395
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2023-02-16 22:23:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+  str = str.toLowerCase()
 	let splitString = str.split("");
 	let reverseArray = splitString.reverse();
 	let str2 = reverseArray.join("");
+	str2 = str2.toLowerCase()
 	let message = false;
 	if(str == str2){
 		message = true;
